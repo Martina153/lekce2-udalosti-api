faebc71fda6d51eab5c4fa18efaa4130d8a2d14f
Martina_Harsanikova <martinaharsanikova@gmail.com>
2023-01-28 16:05:12 +0100

    ok

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // tady je místo pro náš program
+
+
+let odstavec = document.createElement("p");
+let text = document.createTextNode("V elipse spi lev.");
+odstavec.appendChild(text);
+odstavec.style.fontSize = "16px";
+document.body.appendChild(odstavec);
+
+let tlacitko = document.createElement("button");
+let buttonText = document.createTextNode("červený po kliknutí");
+tlacitko.appendChild(buttonText);
+document.body.appendChild(tlacitko);
+
+odstavec.onmouseover = function() {
+    odstavec.style.fontWeight = "bold";
+  }
+  
