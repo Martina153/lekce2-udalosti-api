8c470d1a87a653ee77c31b3a25c07905dba79f7e
Martina_Harsanikova <martinaharsanikova@gmail.com>
2023-01-30 22:02:44 +0100

    hello

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// tady je místo pro náš program
+// Domaci ukol
 
 
 let odstavec = document.createElement("p");
@@ -15,4 +15,83 @@ document.body.appendChild(tlacitko);
 odstavec.onmouseover = function() {
     odstavec.style.fontWeight = "bold";
   }
-  
+
+let tlacitko2 = document.createElement("button");
+let buttonText2 = document.createTextNode("větší po kliknutí");
+tlacitko2.appendChild(buttonText2);
+document.body.appendChild(tlacitko2);
+
+
+tlacitko.addEventListener("click", function() {
+  odstavec.classList.add('mojaTrieda');
+});
+
+tlacitko2.addEventListener("click", function() {
+  let currentSize = parseInt(window.getComputedStyle(odstavec).fontSize);
+  odstavec.style.fontSize = (currentSize + 1) + "px";
+});
+
+let prehravac = document.querySelector('.mujPrehravac')
+function spustAudio(){
+  prehravac.play()
+}
+
+function zastavAudio(){
+  prehravac.pause()
+}
+
+function resetAudio(){
+  prehravac.currentTime = 0;
+}
+function volume3(){
+  prehravac.volume=1;
+}
+function volume1(){
+  prehravac.volume=0;
+}
+function volume2(){
+  prehravac.volume=0.5;
+}
+
+
+
+
+
+
+
+
+
+/*Praca na lekci
+
+/*let nadpis = document.querySelector('h1')
+nadpis.classList.add('csstrida')
+/** removing CSS clasees*/
+/*let ctverecek = document.querySelector('#ctverecek-1')
+ctverecek.classList.remove('zluty')
+ctverecek.classList.remove('ctverecek')*/
+/*allows me to change the CSS class of the element*/
+/*function zmenBarvu(){
+  let ctverecek = document.querySelector('#ctverecek-1')
+  ctverecek.classList.toggle('mujstyl')
+}
+
+let tlacitko = document.querySelector('.tlacitko')
+/* event needs to be in html to work with event in javascript, in function its our first parameter*/
+/*function zavolejUdalost(udalost){
+  let prvek = udalost.target
+  console.log(prvek)
+  console.log(udalost)
+}
+let prehravac = document.querySelector('.mujPrehravac')
+function spustAudio(){
+  prehravac.play()
+}
+
+function zastavAudio(){
+  prehravac.pause()
+}
+
+function resetAudio(){
+  prehravac.currentTime = 0;
+} */
+
